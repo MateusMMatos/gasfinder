14c2db3d093e039e61d67ebb61055747c0c4fab8
Mateus Matos <mateus.m.matos@hotmail.com>
2024-06-20 23:13:50 -0300

    feat: core/urls

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
 from .views import (
-    CidadeViewSet, LocalizacaoViewSet, UsuarioViewSet, PostoCombustivelViewSet,
-    TipoCombustivelViewSet, PrecoCombustivelViewSet, FotoVerificacaoViewSet,
-    AvaliacaoViewSet, ComentarioViewSet, BorrachariaViewSet, OficinaMecanicaViewSet
+    CidadeViewSet, LocalizacaoViewSet, UsuarioViewSet, PostoCombustivelViewSet, TipoCombustivelViewSet,
+    PrecoCombustivelViewSet, FotoVerificacaoViewSet, AvaliacaoViewSet, ComentarioViewSet,
+    BorrachariaViewSet, OficinaMecanicaViewSet, home_view, discounts_view, register_view,
+    login_view, logout_view
 )
-
 
 router = DefaultRouter()
 router.register(r'cidades', CidadeViewSet)
@@ -15,13 +14,17 @@ router.register(r'usuarios', UsuarioViewSet)
 router.register(r'postos', PostoCombustivelViewSet)
 router.register(r'tipos_combustivel', TipoCombustivelViewSet)
 router.register(r'precos_combustivel', PrecoCombustivelViewSet)
-router.register(r'fotos_verificacao', FotoVerificacaoViewSet)
+router.register(r'foto_verificacao', FotoVerificacaoViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 router.register(r'borracharias', BorrachariaViewSet)
 router.register(r'oficinas_mecanicas', OficinaMecanicaViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', views.home, name='home'),  # URL para a view 'home'
+    path('', home_view, name='home'),
+    path('descontos/', discounts_view, name='descontos'),
+    path('registrar/', register_view, name='registrar'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('api/', include(router.urls)),
 ]
