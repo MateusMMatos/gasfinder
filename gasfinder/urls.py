af89a0f064afcdfd97fdc2b75d61c397b8befc61
Mateus Matos <mateus.m.matos@hotmail.com>
2024-06-20 23:14:26 -0300

    feat: gasfinder/urls

--- gasfinder/urls.py
+++ gasfinder/urls.py
@@ -1,8 +1,10 @@
-# gasfinder/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'core.views.custom_404'
+handler500 = 'core.views.custom_500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Incluindo as URLs do aplicativo 'core'
+    path('', include('core.urls')),
 ]
